2255e476152b2893df0effd45240b184081040ff
Utkarsh Agarwal <35272322+utkarsh2019@users.noreply.github.com>
2018-11-27 20:27:56 -0500

    Name setting and content page done and added

--- app/src/main/java/com/project/bitcoinconverter/MainActivity.java
+++ app/src/main/java/com/project/bitcoinconverter/MainActivity.java
@@ -1,5 +1,6 @@
-package com.project.bitcoinconvert;
+package com.project.bitcoinconverter;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.project.bitcoinconvert.R;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +29,56 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        final String filename = "BitcoinName";
+        File directory;
+        if (filename.isEmpty()){
+            directory = getFilesDir();
+        }
+        else {
+            directory = getDir(filename,MODE_PRIVATE);
+        }
+        File [] files = directory.listFiles();
+        try {
+            FileInputStream fis = openFileInput(filename);
+            Scanner sc = new Scanner(fis);
+            sc.useDelimiter("\\Z");
+            String content = sc.next();
+            if(content != null) {
+                userName = content;
+            }
+            sc.close();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(userName == null) {
+            setContentView(R.layout.activity_main);
+            Button submitName = (Button) findViewById(R.id.submitName);
+            final EditText getName = (EditText) findViewById(R.id.nameText);
+
+            submitName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    userName = getName.getText().toString().trim();
+                    try {
+                        FileOutputStream fos = openFileOutput(filename,Context.MODE_PRIVATE);
+                        fos.write(userName.getBytes());
+                        fos.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    setGUI();
+                }
+            });
+        }
+        else {
+            setGUI();
+        }
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +110,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setGUI() {
+        setContentView(R.layout.content_main);
+        TextView welcomeMessage = (TextView) findViewById(R.id.displayName);
+        welcomeMessage.setText("Welcome "+userName+"!");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setContentView(R.layout.content_main);
+        TextView welcomeMessage = (TextView) findViewById(R.id.displayName);
+        welcomeMessage.setText("Welcome "+userName+"!");
+    }
+
+    @Override
+    public void onStop() {
+
+        super.onStop();
     }
 }
